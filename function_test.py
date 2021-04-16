c846a2301cee1089e3b11d9d83a1054e74ae699b
PtCu <1365699009@qq.com>
2021-04-16 13:41:05 +0800

    Functional test now checks we can input a to-do item

--- function_test.py
+++ function_test.py
@@ -1,19 +1,49 @@
-from selenium import webdriver #(1)
+# 功能测
+from selenium import webdriver  # (1)
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVistorTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.browser=webdriver.Firefox()
+        self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+    # def test_can_start_a_list_and_retrieve_it_later(self):
+    #     self.browser.get('http://127.0.0.1:8000')
+    #     self.assertIn('To-Do', self.browser.title)
+    #     self.fail('Finish the test!')
 
-if __name__=='__main__':
+    def test_can_start_a_list_and_retrieve_it_later(self):
+            # Edith has heard about a cool new online to-do app. She goes
+            # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test')
+
+
+if __name__ == '__main__':
     unittest.main(warnings='ignore')
 
 # browser=webdriver.Firefox()     #(2)
